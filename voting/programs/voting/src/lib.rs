4ad98ce4626f8d133d6d8367316033002ee11f89
Adarsh Sharma <dev.adarsh286@gmail.com>
2025-12-21 06:28:34 +0000

    new

--- voting/programs/voting/src/lib.rs
+++ voting/programs/voting/src/lib.rs
@@ -6,11 +6,38 @@ declare_id!("E7Ha7cJQCddaNtqMA2okwJd12rHU2uUWfJMGEtNjswuk");
 pub mod voting {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize_poll(_ctx: Context<IntializePoll>, _poll_id: u64) -> Result<()> {
         Ok(())
     }
 }
 
+
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(poll_id: u64)] //passing poll_id as instruction argument
+pub struct IntializePoll<'info> {
+    //signer account
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    //poll account
+    #[account(̥init,
+        payer= payer,
+        space= 8  + Poll::InitSpace, //8 bytes for account discriminator + space needed for Poll struct
+        seeds= [poll_id.to_le_bytes().as_ref()]
+        bump, //program derived address bump
+    )]
+    pub poll: Account<'info, Poll>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)] //auto calculate space needed
+pub struct Poll{
+    pub poll_id: u64,
+    #[max_len(280)]
+    pub description: String,
+    pub poll_start: u64,
+    pub poll_end: u64,
+    pub candidate_amount: u64,
+}
